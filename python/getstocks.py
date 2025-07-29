c6d2914a15888afbecdf2160020c456ffaeabee9
Patrick Rinke <patrick.rinke@rinke-it.de>
2025-07-29 10:53:17 +0200

    Update getstocks.py

--- python/getstocks.py
+++ python/getstocks.py
@@ -22,33 +22,35 @@
 #  #########################################################################
 #
 
+import config
 import requests
 import json
 import pandas 
-
-API_Endpoint_Url = ''
-API_Endpoint_Port = 5001
-
-API_Username = ''
-API_Password = ''
+import urllib3
+from pathlib import Path
 
 limit = 500
-filename = "d:/temp/stocks.csv"
 
-stock = []
+data = []
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+Path(config.exportfolder).mkdir(parents=True, exist_ok=True)
 
 # login
 login_params = {
-    'username': API_Username,
-    'password': API_Password,
+    'username': config.API_Username,
+    'password': config.API_Password,
     'grant_type': "password"
 }
 
-login_request = json.loads(requests.post(API_Endpoint_Url + ':' + str(API_Endpoint_Port) + '/token', json = login_params).text)
+login_request = json.loads(requests.post(config.API_Endpoint_Url + ':' + str(config.API_Endpoint_Port) + '/token', json = login_params, verify = config.API_Verify_SSL).text)
 
 if(login_request["access_token"] == "") :
     print("no access token")
     exit()
+
+print("login successful")
 
 # Get stocks
 request_header = {
@@ -56,17 +58,23 @@ request_header = {
 }
 
 pagination_token = ""
+chunk = 0
 
 while(True) :
-    stock_request = json.loads(requests.get(API_Endpoint_Url + ':' + str(API_Endpoint_Port) + '/api/v2/stock?pagination_token='+pagination_token+'&max_result=' +str(limit), headers = request_header).text)    
-    stock.extend(stock_request["data"])
-    
-    if(not "next_token" in stock_request) :
-        break
-        
-    pagination_token = stock_request["next_token"]
+    chunk += 1
+    print("retrieve data (chunk " + str(chunk) + ", chunksize " + str(limit) +")")    
 
-data = pandas.DataFrame(stock)
-data.to_csv(filename, header=True, index=False)
+    request = json.loads(requests.get(config.API_Endpoint_Url + ':' + str(config.API_Endpoint_Port) + '/api/v2/stock?pagination_token='+pagination_token+'&max_result=' +str(limit), headers = request_header, verify = config.API_Verify_SSL).text)    
+    data.extend(request["data"])
+
+    if(not "next_token" in request) :
+        break
+
+    pagination_token = request["next_token"]
+
+print("processing " + str(len(data)) + " articles")
+
+data = pandas.DataFrame(data)
+data.to_csv(config.exportfolder + config.stocks_filename, header=True, index=False, sep=config.csv_seperator)
 
 print("...Done")
