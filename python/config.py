721b4ec67eb57367799eaf84690e3b91352b871d
Patrick Rinke <patrick.rinke@rinke-it.de>
2025-07-29 10:52:54 +0200

    Create config.py

---
+++ python/config.py
@@ -0,0 +1,21 @@
+API_Endpoint_Url = ''
+API_Endpoint_Port = 5001
+
+API_Username = ''
+API_Password = ''
+
+API_Verify_SSL = False
+
+limit = 500
+
+exportfolder = "d:/temp/exportdata/"
+filename = "articles.csv"
+imagefolder = "images/"
+csv_seperator = ";"
+
+# articles
+articles_download_images = True
+articles_filename = "articles.csv"
+
+# stocks
+stocks_filename = "stocks.csv"
