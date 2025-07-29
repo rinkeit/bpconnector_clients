6c56848d3bf377572f8010c3c775818d556c43b2
Patrick Rinke <patrick.rinke@rinke-it.de>
2025-07-29 11:26:20 +0200

    Create config.ps1

---
+++ powershell/config.ps1
@@ -0,0 +1,18 @@
+$API_Endpoint_Url = ''
+$API_Endpoint_Port = 5001
+
+$API_Username = ''
+$API_Password = ''
+
+$API_Verify_SSL = $false
+
+$exportfolder = "d:/temp/exportdata/"
+$imagefolder = "images/"
+$csv_seperator = ";"
+
+# articles
+$articles_download_images = $true
+$articles_filename = "articles.csv"
+
+# stocks
+$stocks_filename = "stocks.csv"
